7959f8026299d88971695581ea7b11e434239fd8
RMolania <ramyar.molania@gmail.com>
2024-04-08 15:09:25 +1000

    update output file names

--- R/createPrPsByMnn.R
+++ R/createPrPsByMnn.R
@@ -328,17 +328,13 @@ createPrPsByMnn <- function(
         if (!'un.supervised' %in% names(se.obj@metadata[['PRPS']])) {
             se.obj@metadata[['PRPS']][['un.supervised']] <- list()
         }
-        ## check if metadata PRPS already exist for supervised
-        if (!'mnn' %in% names(se.obj@metadata[['PRPS']][['un.supervised']])) {
-            se.obj@metadata[['PRPS']][['un.supervised']][['mnn']] <- list()
-        }
         ## check if prps.set.name already exists in the PRPS$supervised slot
-        if (!prps.set.name %in% names(se.obj@metadata[['PRPS']][['un.supervised']][['mnn']])) {
-            se.obj@metadata[['PRPS']][['un.supervised']][['mnn']][[prps.set.name]] <- list()
+        if (!prps.set.name %in% names(se.obj@metadata[['PRPS']][['un.supervised']])) {
+            se.obj@metadata[['PRPS']][['un.supervised']][[prps.set.name]] <- list()
         }
         ## check if out.put.name already exists in the PRPS$supervised$prps.set.name slot
-        if (!out.put.name %in% names(se.obj@metadata[['PRPS']][['un.supervised']][['mnn']][[prps.set.name]])) {
-            se.obj@metadata[['PRPS']][['un.supervised']][['mnn']][[prps.set.name]][[out.put.name]] <- prps.data
+        if (!out.put.name %in% names(se.obj@metadata[['PRPS']][['un.supervised']][[prps.set.name]])) {
+            se.obj@metadata[['PRPS']][['un.supervised']][[prps.set.name]][[out.put.name]] <- prps.data
         }
         printColoredMessage(message = '------------The unsupervisedPRPSmnn function finished.',
                             color = 'white',
--- R/findNcgAcrossSamples.R
+++ R/findNcgAcrossSamples.R
@@ -888,8 +888,15 @@ findNcgAcrossSamples <- function(
             color = 'blue',
             verbose = verbose)
         ## Check if metadata NCG already exists
+        ## Check if metadata NCG already exists
         if(length(se.obj@metadata$NCG) == 0 ) {
             se.obj@metadata[['NCG']] <- list()
+        }
+        if(!'supervised' %in% names(se.obj@metadata[['NCG']])){
+            se.obj@metadata[['NCG']][['supervised']] <- list()
+        }
+        if(!output.name %in% names(se.obj@metadata[['NCG']][['supervised']])){
+            se.obj@metadata[['NCG']][['supervised']][[output.name]] <- list()
         }
         se.obj@metadata[['NCG']][['supervised']][[output.name]] <- ncg.selected
         printColoredMessage(
