887a59d0d01f75b20e6ccca444b195f3d4160543
RMolania <ramyar.molania@gmail.com>
2024-05-05 16:49:41 +1000

    update assay names

--- R/plotRleVariable.R
+++ R/plotRleVariable.R
@@ -97,8 +97,8 @@ plotRleVariable <- function(
 
     # assays ####
     if (length(assay.names) == 1 && assay.names == 'all') {
-        assay.names <- as.factor(names(assays(se.obj)))
-    } else assay.names <- factor(x = assay.names, levels = assay.names)
+        assay.names <- factor(x = names(assays(se.obj)), levels = names(assays(se.obj)))
+    } else  assay.names <- factor(x = assay.names , levels = assay.names)
     if(!sum(assay.names %in% names(assays(se.obj))) == length(assay.names)){
         stop('The "assay.names" cannot be found in the SummarizedExperiment object.')
     }
